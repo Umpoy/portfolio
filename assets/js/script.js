877370c40f8af25a713cb219d5ba9bb29e0456a9
Umpoy <iumpoy@yahoo.com>
2018-01-29 17:25:11 -0800

    added porjects

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.cubetube_live').on('click', function () {
+        window.open('https://thecubetube.com/', '_blank')
+    })
+    $('.cubetube_github').on('click', function () {
+        window.open('https://github.com/AnthonySorise/The-Cube-Tube', '_blank')
+    })
+    $('.ygo_live').on('click', function () {
+        window.open('memory_match/index.html')
+    });
+    $('.ygo_github').on('click', function () {
+        window.open('https://github.com/Umpoy/memory_match_oop')
+    });
+    $('.simon_live').on('click', function () {
+        window.open('simon_says/index.html')
+    });
+    $('.simon_github').on('click', function () {
+        window.open('https://github.com/Umpoy/simon_says')
+    });
+});
