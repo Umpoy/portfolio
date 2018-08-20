b275e0748c7e417ed2c78b4d1f3d89431ec94164
Umpoy <iumpoy@yahoo.com>
2018-08-19 17:04:48 -0700

    included fonts

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+// navbar and logo switch related with scroll position
+$(window).on('scroll', function () {
+    if ($(window).width() > 1024) {
+        if ($(document).scrollTop() > 150) {
+            setNavbarLight();
+        } else {
+            setNavbarTransparent();
+        }
+    }
+});
+
+// navbar and logo switch related with screen width
+function toggleNavbar() {
+    if (($(window).width() > 1024) && ($(document).scrollTop() <= 150)) {
+        setNavbarTransparent();
+    } else {
+        setNavbarLight();
+    }
+}
+
+toggleNavbar();
+
+$(window).resize(function () {
+    toggleNavbar();
+});
+
+/* navbar setting functions */
+function setNavbarLight() {
+    $('.navbar').addClass('navbar-light');
+    $('.navbar-brand img').attr('src', 'assets/img/loop-logo.png');
+}
+
+function setNavbarTransparent() {
+    $('.navbar').removeClass('navbar-light');
+    $('.navbar-brand img').attr('src', 'assets/img/loop-logo-white.png');
+}
