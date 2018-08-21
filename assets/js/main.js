c3016c72ed3e36e2d39095f10c54af41540a2289
Umpoy <iumpoy@yahoo.com>
2018-08-21 13:56:58 -0700

    added parallax quote and contact info

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-
+$(document).ready(function () {
+    render_info();
+});
 // navbar and logo switch related with scroll position
 $(window).on('scroll', function () {
     if ($(window).width() > 1024) {
@@ -34,4 +36,11 @@ function setNavbarLight() {
 function setNavbarTransparent() {
     $('.navbar').removeClass('navbar-dark bg-dark ');
     $('.navbar-brand img').attr('src', 'assets/img/loop-logo-white.png');
+}
+
+function render_info() {
+    var phone_number = '7144725319';
+    var email = 'audrianmanly@gmail.com';
+    $('.phone_number').text("714-472-5319").attr('href', 'tel:1' + phone_number);
+    $('.email').text(email).attr('href', 'mailto:' + email)
 }
